8895136590b5991fee829ef04c1112f02e46cfdd
matheus-biesek <md.biesek@gmail.com>
2024-07-21 01:51:07 -0300

    Mudança dor cors para produção aws free tier

--- src/main/java/com/closing/inventory/infra/cors/CorsConfig.java
+++ src/main/java/com/closing/inventory/infra/cors/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT")
+                .allowedOrigins("http://3.17.160.44:8080")
+                .allowedMethods("GET", "POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
