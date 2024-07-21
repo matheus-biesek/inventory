f1535ec8fd1e20afc1d5b5de2f7dbf18a6ab311b
matheus-biesek <md.biesek@gmail.com>
2024-07-21 02:53:01 -0300

    javascript ip

--- src/main/java/com/closing/inventory/infra/cors/CorsConfig.java
+++ src/main/java/com/closing/inventory/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://3.17.160.44:8080/")
+                .allowedOrigins("https://3.17.160.44:8080", "https://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
